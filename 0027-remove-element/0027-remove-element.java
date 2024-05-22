f25e9f5f52409e76c6ecce142736d173cd5a9b2c
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-05-22 09:53:01 +0530

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,8 +3,8 @@ class Solution {
         int k=0;
         for(int i=0;i<nums.length;i++){
                 if (nums[i] != val) {
-                nums[k] = nums[i]; // Copy non-'val' element to the current position
-                k++; // Move to the next position
+                nums[k] = nums[i]; 
+                k++;
             }
             
         }
