7ca5192804cb9644b4588e4cfc4ee8cddd72283a
Abhay Gupta <111756624+ebhay@users.noreply.github.com>
2025-03-12 22:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -6,15 +6,13 @@ public:
         for(int i=0; i<s.length();i++){
             char ch = s[i];
 
-            if(ch=='[' || ch=='{' || ch=='(')
-                str.push(ch);
-            else if(!str.empty() && (ch==']' && str.top()=='[' || ch=='}' && str.top()=='{' || ch==')' && str.top()=='('))
+            
+           if(!str.empty() && (ch==']' && str.top()=='[' || ch=='}' && str.top()=='{' || ch==')' && str.top()=='('))
             {
                 str.pop();
             }
-            else 
-            return false;
-
+            else
+                str.push(ch);
         }
         return str.empty();    
     }
