7d2346eff69d65a61392ed521d9a630db6e9ddcb
Abhay Gupta <111756624+ebhay@users.noreply.github.com>
2025-06-09 19:52:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -1,63 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     void sort012(vector<int>& arr) {
         // code here
-        int l=0,m=0,h=arr.size()-1;
+        int m=0,l=0,h=arr.size()-1;
         while(m<=h){
-            if(arr[m]==0){
-                arr[m]=arr[l];
-                arr[l]=0;
-                m++,l++;
-            }
-            else if(arr[m]==1){
+            if(arr[m]==1){
                 m++;
             }
+            else if(arr[m]==0){
+                int temp=arr[l];
+                arr[l]=arr[m];
+                arr[m]=temp;
+                m++,l++;
+            }
             else{
-                arr[m]=arr[h];
-                arr[h]=2;
+                int temp=arr[h];
+                arr[h]=arr[m];
+                arr[m]=temp;
                 h--;
             }
         }
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        vector<int> a;
-        string input;
-        int num;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-        Solution ob;
-        ob.sort012(a);
-
-        int n = a.size();
-        for (int i = 0; i < n; i++) {
-            cout << a[i] << " ";
-        }
-
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
