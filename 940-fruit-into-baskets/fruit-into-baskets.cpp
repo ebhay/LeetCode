6724a64c22375f79c57795657474ff2879495a64
Abhay Gupta <111756624+ebhay@users.noreply.github.com>
2025-07-04 11:42:50 +0530

    Time: 35 ms (74.63%) | Memory: 74.9 MB (88.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& arr) {
-        int b1=arr[0],b2=-1;
-        int f1=1,f2=0;
-        int maxf=1;
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]==b1)
-            f1++;
-            else if(arr[i]==b2 || b2==-1)
-            {
-                b2=arr[i];
-                f2++;
+        int l = 0, r = 0, n = arr.size(), len = 0;
+        unordered_map<int, int> mp; // val -> freq
+
+        while (r < n) {
+            mp[arr[r]]++;
+
+            while (mp.size() > 2) { // Shrink window if more than 2 types
+                mp[arr[l]]--;
+                if (mp[arr[l]] == 0)
+                    mp.erase(arr[l]);
+                l++;
             }
-            else//dono type ka fruite nahi hai {
-            {
-                b1=arr[i];
-                f1=1;
-                b2=arr[i-1];
-                f2=1;
-                int k = i - 2; // Start checking from two elements before current
-                while (k >= 0 && arr[k] == b2) {
-                    f2++;
-                    k--;
-                }
-            }
-            maxf=max(maxf,f1+f2);
+
+            len = max(len, r - l + 1);
+            r++;
         }
-        return maxf;
+
+        return len;
     }
 };
