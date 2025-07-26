dd069691a96182c448267c178ed363aa43bb091c
Abhay Gupta <111756624+ebhay@users.noreply.github.com>
2025-07-26 22:00:20 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (22.50%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,31 +1,33 @@
 class Solution {
-    public int myAtoi(String s) {
-         s = s.trim();
-        if (s.length() == 0) return 0;
+    public int myAtoi(String a) {
+        
+        String s=a.trim();
+        if(s.length()==0)
+        return 0;
+        int sign=1;
+        int i=0;
+        if(s.charAt(i)=='-'){
+            sign=-1;i++;}
+        else if(s.charAt(i)=='+'){
+            sign=1;i++;}
 
-        int sign = 1, res = 0, i = 0;
-
-        // Handle sign
-        if (s.charAt(i) == '-') {
-            sign = -1;
-            i++;
-        } else if (s.charAt(i) == '+') {
-            i++;
-        }
-
-        // Convert digits
-        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            int digit = s.charAt(i) - '0';
-
-            // Check overflow
-            if (res > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int num=0;
+        while(i<s.length() && s.charAt(i)-'0'>=0 && s.charAt(i)-'0'<=9 ){
+            if(num>(Integer.MAX_VALUE-(s.charAt(i)-'0'))/10){
+                if(sign==-1)
+                return Integer.MIN_VALUE;
+                else
+                return Integer.MAX_VALUE;
             }
 
-            res = res * 10 + digit;
+            num=num*10+(s.charAt(i)-'0');
             i++;
+            
+            
         }
+        if(sign==-1)
+        return num*-1;
+        return num;
 
-        return res * sign;
     }
 }
